aef4a90032ea96a16c02993217af7f64b03f663e
babumg1210 <babumg1210@outlook.com>
2024-12-10 22:20:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "powershell-grp"
+    storage_account_name  = "appstore400089891210"
+    container_name        = "subbu"
+    key                   = "terraform.tfstate"
+  }
+}
 provider "azurerm" {
   features {}
 
