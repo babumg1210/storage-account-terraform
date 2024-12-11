460e0f705198dc193422809161a7b6448f25388b
babumg1210 <babumg1210@outlook.com>
2024-12-11 08:54:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  client_id       = "5eccd7fc-aed1-446a-a81e-aa66c5584316"  # appId from the JSON output
+  client_secret   = "bz88Q~4OJ~O-.67ptF4Sbyf6~wEP-d9SEyTk9adPv"  # password from the JSON output
+  tenant_id       = "c7de2ec6-e7a9-41e7-99e8-e6ff52f3a8d0"  # tenant from the JSON output
+  subscription_id = "ae1b5111-4f4f-477d-b4d4-419fd8cbc789"  # Your Azure subscription ID
 }
 
 module "storage_account" {
