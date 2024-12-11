020d1a1b1b42f3b9f9197dc9272228413418d419
babumg1210 <babumg1210@outlook.com>
2024-12-11 19:36:21 +0530

    Update main.tf

--- Storage-account/main.tf
+++ Storage-account/main.tf
@@ -9,12 +9,25 @@ resource "azurerm_storage_account" "storage_account" {
     environment = var.environment
   }
 }
-
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+# Resource-1: Create WebTier Subnet
+resource "azurerm_subnet" "websubnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_id  
+}
 resource "azurerm_private_endpoint" "example" {
   name                = "${var.storage_account_name}-endpoint"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.id
 
   private_service_connection {
     name                           = "example-privateserviceconnection"
@@ -26,12 +39,12 @@ resource "azurerm_private_endpoint" "example" {
 
 resource "azurerm_private_dns_zone" "example" {
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "example" {
   name                  = "${var.vnet_name}-vnetlink"
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.example.name
   virtual_network_id    = var.vnet_id
 }
